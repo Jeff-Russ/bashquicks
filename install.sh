5c25e3155ee4476f0982da939aaa3644f4161b56
Jeff Russ <jeffreylynnruss@gmail.com>
2015-12-22 06:18:01 +0000

    started new bash_prompt

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # install.sh 
 
+# TODO touch ~/.bash_prompt; ~/.bash_prompt < cat ~/bashful_rails/resources/bashrc_prompt 
+# or something like that
+
 # adds all scripts and aliases to environment by appending the .bashrc file in ~/
 
 TOKEN="#bashful_rails_begin"
